951ff9da732a9f9114fc9946aa4b0a6d6f564485
bakasura980 <lyubo@limechain.tech>
2020-12-02 20:20:07 +0200

    Fix tokens calculation to work with Bignumbers

--- src/api/controllers/user-controller.ts
+++ src/api/controllers/user-controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express'
+import { BigNumber } from 'ethers'
 
 import { ReMeApi, RRPApi } from '../../external-apis'
 import { DistributionService } from '../../services'
@@ -10,11 +11,12 @@ class UsersController {
         const remeUser = await ReMeApi.getUser(res.locals.token, res.locals.tokenInfo.id)
         const user = await Users.getByEmail(remeUser.username)
 
+        const loadedTokens = BigNumber.from(user.loadedTokens)
         const totalClaimed = await DistributionService.getTotalClaimed(user.ethAddress)
-        const tokensForClaiming = user.loadedTokens - totalClaimed
+        const tokensForClaiming = loadedTokens.sub(totalClaimed)
 
-        const rrpBalance = await RRPApi.getReferralBalance(res.locals.token, user.ethAddress)
-        const incomingTokens = (rrpBalance + user.signupTokens) - user.loadedTokens
+        const rrpBalance = BigNumber.from(await RRPApi.getReferralBalance(res.locals.token, user.ethAddress))
+        const incomingTokens = rrpBalance.add(BigNumber.from(user.signupTokens)).sub(loadedTokens)
 
         res.send({
             email: user.email,
@@ -25,10 +27,10 @@ class UsersController {
             },
             earnedTokens: {
                 signup: user.signupTokens,
-                referral: rrpBalance
+                referral: rrpBalance.toString()
             },
-            incomingTokens,
-            tokensForClaiming
+            incomingTokens: incomingTokens.toString(),
+            tokensForClaiming: tokensForClaiming.toString()
         })
     }
 
--- src/services/distribution-service/index.ts
+++ src/services/distribution-service/index.ts
@@ -18,8 +18,8 @@ class DistributionService {
         return DistributionService.instance
     }
 
-    public async getTotalClaimed (address: string): Promise<number> {
-        return this.contract.totalClaimed(address)
+    public async getTotalClaimed (address: string): Promise<any> {
+        return this.contract.claimedTokensPerUser(address)
     }
 
     public async updateRootHash (hash: string): Promise<void> {
